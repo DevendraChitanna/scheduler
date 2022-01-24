149304222cf939f88ba34e1fb3db7c62f660158b
DevendraChitanna <devendrachitanna@gmail.com>
2022-01-24 13:04:48 -0500

    Completed InterviewerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const { interviewers, setInterviewer, interviewer } = props;
+
+  const interviewerList = interviewers.map((item) => {
+    return <InterviewerListItem key={item.id}
+      name={item.name}
+      avatar={item.avatar}
+      selected={item.id === interviewer}
+      setInterviewer={setInterviewer}
+      id={item.id}
+    />
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerList}
+      </ul>
+    </section >
+  );
+}
